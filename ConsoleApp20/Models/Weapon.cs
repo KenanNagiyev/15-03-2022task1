28af99ecdffccfbf02c3cf0b945f598b1fd4dc92
KenanNagiyev <788lnmp@code.edu.az>
2022-03-16 08:51:08 +0400

    new file

---
+++ ConsoleApp20/Models/Weapon.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp20.Models
+{
+    class Weapon
+    {
+        public int bulletCap;
+        public int bulletCount;
+        public double time;
+        public bool auto = true;
+        public Weapon()
+        {
+
+        }
+
+        public Weapon(int bulletCap, int bulletCount, double time, bool auto)
+        {
+            this.bulletCap = bulletCap;
+            this.bulletCount = bulletCount;
+            this.time = time;
+            this.auto = auto;
+            if (bulletCount > bulletCap)
+            {
+                Console.WriteLine("Xeberdarliq:gulle sayi tutumdan cox ola bilmez");
+            }
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"gulle tutumu:{ bulletCap}");
+            Console.WriteLine($"gulle sayi:{ bulletCount}");
+            Console.WriteLine($"vaxti:{ time}");
+        }
+        public void Shoot()
+        {
+
+
+            if (bulletCount <= 0)
+            {
+                Console.WriteLine("Gulle yoxdur");
+            }
+            else if (auto == true)
+            {
+
+                bulletCount--;
+
+                if (bulletCount > 0)
+                {
+                    bulletCount--;
+                    Console.WriteLine("part");
+
+                }
+                else
+                {
+                    Console.WriteLine("Gulle yoxdur");
+                }
+
+            }
+        }
+
+        public void Fire()
+        {
+            double shootbullets = 0;
+            if (bulletCount == 0)
+            {
+                Console.WriteLine("gulle  yoxdur");
+                return;
+            }
+            if (auto)
+            {
+                while (bulletCount > 0)
+                {
+                    bulletCount--;
+                    shootbullets++;
+                    Console.WriteLine(shootbullets);
+                }
+            }
+            else
+            {
+                while (bulletCount > 0)
+                {
+                    bulletCount--;
+                    shootbullets++;
+                    Console.WriteLine(shootbullets);
+                }
+            }
+            shootbullets *= (bulletCap / time);
+            Console.WriteLine($"gulle bu qeder vaxta atildi:{shootbullets}");
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine(bulletCap - bulletCount);
+        }
+        public void Reload()
+        {
+            if (bulletCap - bulletCount == 0)
+            {
+                Console.WriteLine("doludur");
+            }
+            else
+            {
+                Console.WriteLine(bulletCap - bulletCount);
+                bulletCount = bulletCap;
+            }
+        }
+    }
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ConsoleApp20.Models;
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon w = new Weapon(10, 4, 4, true);
+
+
+            int choose = 0;
+            while (choose != 4)
+            {
+                Console.WriteLine("choose");
+                choose = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("--------------");
+                switch (choose)
+                {
+                    case 0:
+                        {
+                            w.Shoot();
+                        }
+                        break;
+                    case 1:
+                        {
+                            w.Fire();
+                        }
+                        break;
+                    case 2:
+                        {
+                            w.GetRemainBulletCount();
+                        }
+                        break;
+                    case 3:
+                        {
+                            w.Reload();
+                        }
+                        break;
+                    case 4:
+                        {
+                            w.GetInfo();
+                        }
+                        break;
+
+                    default:
+                        {
+                            Console.WriteLine("sef melumatdir");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
